17a8022af012aa75fad5943dd19c3067ef5ee522
Timéo Deversin <timeo.deversin15@gmail.com>
2026-08-01 22:04:53 +0200

    Ajoute l'option --symboles pour enrichir le jeu de caractères

--- Generator.py
+++ Generator.py
@@ -2,12 +2,16 @@ import secrets
 import string
 import argparse
 parser = argparse.ArgumentParser(description="Générateur de mot de passe")
-parser.add_argument("-l", "--longueur", type=int, default=12, help="La longueur du mot de passe")
+parser.add_argument("-l", "--longueur", type=int, default=12, help="La longueur du mots de passe")
+parser.add_argument("-s", "--symboles", action="store_true", help="Rajout de symbole dans le mots de passe")
 args = parser.parse_args()
 
 alphabet = string.ascii_letters + string.digits
-longueur = args.longueur  
+longueur = args.longueur 
 password = ""
+
+if args.symboles:
+    alphabet += string.punctuation
 
 for i in range(longueur):
     password = password + secrets.choice(alphabet)
